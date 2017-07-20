5f553e09f6d13d8067f5028478dd86fc4d8576e0
Mike Boice <mike.boice@netapp.com>
2017-07-19 21:21:20 -0600

    Add splash and start screens

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+
+    public float autoLoadNextLevelAfter;
+
+    public void Start()
+    {
+        Invoke("LoadNextLevel", autoLoadNextLevelAfter);
+    }
+
+    public void LoadLevel(string name)
+    {
+        Debug.Log("Level load requested for:" + name);
+        SceneManager.LoadScene(name);
+    }
+
+    public void QuitRequest()
+    {
+        Debug.Log("I want to quit");
+        Application.Quit();
+    }
+
+    public void LoadNextLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+}
