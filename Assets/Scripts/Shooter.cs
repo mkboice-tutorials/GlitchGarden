309b683b716f9fb9848aa6afcb7a5ff9c46004df
Mike Boice <mike.boice@netapp.com>
2017-09-03 14:30:05 -0600

    Have defenders shoot when attackers are in it's lane and in front of the defender

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -8,14 +8,60 @@ public class Shooter : MonoBehaviour
     public GameObject projectile, gun;
 
     private GameObject projectileParent;
+    private Animator animator;
+    private Spawner myLaneSpawner;
 
     void Start()
     {
+        // Creates a parent if necessary
         projectileParent = GameObject.Find("Projectiles");
         if (!projectileParent)
         {
             projectileParent = new GameObject("Projectiles");
         }
+
+        animator = GameObject.FindObjectOfType<Animator>();
+
+        SetMyLaneSpawner();
+    }
+
+    void Update()
+    {
+        animator.SetBool("isAttacking", IsAttackerAheadInLane());
+    }
+
+    // Look through all spawners and set myLaneSpawner if found
+    void SetMyLaneSpawner()
+    {
+        Spawner[] spawnerArray = GameObject.FindObjectsOfType<Spawner>();
+        foreach (Spawner spawner in spawnerArray)
+        {
+            if (Mathf.Approximately(spawner.transform.position.y, transform.position.y))
+            {
+                myLaneSpawner = spawner;
+                return;
+            }
+        }
+        Debug.LogError(name + " can't find spawner in lane");
+    }
+
+    bool IsAttackerAheadInLane()
+    {
+        // Exit if not attackers in lane
+        if (myLaneSpawner.transform.childCount <= 0)
+        {
+            return false;
+        }
+
+        // If there are attackers, are they ahead?
+        foreach (Transform attacker in myLaneSpawner.transform)
+        {
+            if (attacker.transform.position.x > transform.position.x)
+            {
+                return true;
+            }
+        }
+        return false;
     }
 
     private void Fire()
