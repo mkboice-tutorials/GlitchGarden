c1373ef8b385f19cca28eef910c318b95249429a
Mike Boice <mike.boice@netapp.com>
2017-09-03 11:08:05 -0600

    Add projectile damage

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -18,4 +18,16 @@ public class Projectile : MonoBehaviour
     {
         transform.Translate(Vector3.right * speed * Time.deltaTime);
     }
+
+    void OnTriggerEnter2D(Collider2D obj_collider)
+    {
+        Attacker attacker = obj_collider.gameObject.GetComponent<Attacker>();
+        Health health = obj_collider.gameObject.GetComponent<Health>();
+
+        if (attacker && health)
+        {
+            health.DealDamage(damage);
+            Destroy(gameObject);
+        }
+    }
 }
