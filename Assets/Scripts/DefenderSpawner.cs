99c03444545954f7a01af805c10625484d69d9c9
Mike Boice <mike.boice@netapp.com>
2017-09-03 12:59:34 -0600

    Add defenders to grid

---
+++ Assets/Scripts/DefenderSpawner.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefenderSpawner : MonoBehaviour
+{
+
+    public Camera myCamera;
+
+    private GameObject parent;
+
+    void Start()
+    {
+        parent = GameObject.Find("Defenders");
+        if (!parent)
+        {
+            parent = new GameObject("Defenders");
+        }
+    }
+
+    void OnMouseDown()
+    {
+        Vector2 rawPos = CalculateWorldPointOfMouseClick();
+        Vector2 roundedPos = SnapToGrid(rawPos);
+        GameObject defender = Button.selectedDefender;
+        Quaternion zeroRotation = Quaternion.identity;
+        GameObject newDefender = Instantiate(defender, roundedPos, zeroRotation);
+        newDefender.transform.parent = parent.transform;
+
+    }
+
+    Vector2 SnapToGrid(Vector2 rawWorldPos)
+    {
+        float newX = Mathf.RoundToInt(rawWorldPos.x);
+        float newY = Mathf.RoundToInt(rawWorldPos.y);
+        return new Vector2(newX, newY);
+    }
+
+    Vector2 CalculateWorldPointOfMouseClick()
+    {
+        float mouseX = Input.mousePosition.x;
+        float mouseY = Input.mousePosition.y;
+        float distanceFromCamera = 10f;
+
+        Vector3 weirdTriplet = new Vector3(mouseX, mouseY, distanceFromCamera);
+        Vector2 worldPos = myCamera.ScreenToWorldPoint(weirdTriplet);
+
+        return worldPos;
+    }
+}
