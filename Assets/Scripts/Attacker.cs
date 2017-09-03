ac83b76ce7b103b5c7126dfaaa7e8f5796a20a32
Mike Boice <mike.boice@netapp.com>
2017-09-03 13:27:00 -0600

    Spawn attackers

--- Assets/Scripts/Attacker.cs
+++ Assets/Scripts/Attacker.cs
@@ -6,6 +6,8 @@ using UnityEngine;
 public class Attacker : MonoBehaviour
 {
     private float currentSpeed;
+    [Tooltip("Average number of seconds between appearances")]
+    public float seenEverySeconds;
     private GameObject currentTarget;
     private Animator animator;
 
