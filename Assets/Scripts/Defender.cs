c05aab5ac8f5583ad5149b387c15f4c4e62193b8
Mike Boice <mike.boice@netapp.com>
2017-09-03 19:58:12 -0600

    Generate star currency

--- Assets/Scripts/Defender.cs
+++ Assets/Scripts/Defender.cs
@@ -4,5 +4,16 @@ using UnityEngine;
 
 public class Defender : MonoBehaviour
 {
+    private StarDisplay starDisplay;
+
+    void Start()
+    {
+        starDisplay = GameObject.FindObjectOfType<StarDisplay>();
+    }
+
     // Only being used as a tag for now
+    public void AddStars(int amount)
+    {
+        starDisplay.AddStars(amount);
+    }
 }
