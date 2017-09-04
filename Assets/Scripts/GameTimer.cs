61908a7879cf4976ca26258da9e6b612a874c25d
Mike Boice <mike.boice@netapp.com>
2017-09-03 21:11:21 -0600

    Add level timer

---
+++ Assets/Scripts/GameTimer.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameTimer : MonoBehaviour
+{
+
+    public float levelSeconds = 100;
+
+    private Slider slider;
+    private AudioSource audioSource;
+    private bool isEndOfLevel = false;
+    private LevelManager levelManager;
+    private GameObject winLabel;
+
+    // Use this for initialization
+    void Start()
+    {
+        slider = GetComponent<Slider>();
+        audioSource = GetComponent<AudioSource>();
+        levelManager = GameObject.FindObjectOfType<LevelManager>();
+
+        FindYouWin();
+        winLabel.SetActive(false);
+    }
+
+    void FindYouWin()
+    {
+        winLabel = GameObject.Find("You Win");
+        if (!winLabel)
+        {
+            Debug.LogWarning("Please create you win object");
+        }
+    }
+	
+    // Update is called once per frame
+    void Update()
+    {
+        slider.value = Time.timeSinceLevelLoad / levelSeconds;
+        if (Time.timeSinceLevelLoad >= levelSeconds && !isEndOfLevel)
+        {
+            audioSource.Play();
+            isEndOfLevel = true;
+            winLabel.SetActive(true);
+            Invoke("LoadNextLevel", audioSource.clip.length);
+        }
+    }
+
+    void LoadNextLevel()
+    {
+        levelManager.LoadNextLevel();
+    }
+}
